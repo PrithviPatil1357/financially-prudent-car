4074d977ce16b958f67844cc4df61ebd55aa9063
Prithvi Patil <prithvipatil1357@gmail.com>
2023-02-08 19:42:35 +0530

    cors

--- src/main/java/com/prithvipatil/financiallyprudentcar/FinanciallyPrudentCarApplication.java
+++ src/main/java/com/prithvipatil/financiallyprudentcar/FinanciallyPrudentCarApplication.java
@@ -7,21 +7,23 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+
 public class FinanciallyPrudentCarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanciallyPrudentCarApplication.class, args);
 	}
 
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**")
-//						.allowedOrigins("https://financially-prudent-car.firebaseapp.com")
-//						.allowedMethods("*");
-//			}
-//		};
-//	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000", "https://financially-prudent-car.firebaseapp.com")
+						.allowedMethods("*");
+			}
+		};
+	}
+
 }
