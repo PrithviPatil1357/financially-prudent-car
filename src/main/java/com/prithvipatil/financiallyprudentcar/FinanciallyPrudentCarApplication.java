be42d21657e43b01070860ed5ddb8712b1bd3797
Prithvi Patil <prithvipatil1357@gmail.com>
2023-02-15 22:59:49 +0530

    Changes as currently running on EC2

--- src/main/java/com/prithvipatil/financiallyprudentcar/FinanciallyPrudentCarApplication.java
+++ src/main/java/com/prithvipatil/financiallyprudentcar/FinanciallyPrudentCarApplication.java
@@ -13,15 +13,15 @@ public class FinanciallyPrudentCarApplication {
 		SpringApplication.run(FinanciallyPrudentCarApplication.class, args);
 	}
 
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**")
-//						.allowedOrigins("https://financially-prudent-car.firebaseapp.com")
-//						.allowedMethods("*");
-//			}
-//		};
-//	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000","https://financially-prudent-car.firebaseapp.com","https://financially-prudent-car.web.app")
+						.allowedMethods("*");
+			}
+		};
+	}
 }
